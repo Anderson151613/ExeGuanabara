c15b3cbde6972d6545b4710a2f7042e30a947ee0
Anderson Fernandes <a59399800@gmail.com>
2022-08-22 18:27:00 -0300

    alteado

---
+++ Guanabara/JavaScript/aula01/aula01.js
@@ -0,0 +1,50 @@
+// alert(" Minha primeira mensagem!") Vai aparecer uma msg
+// confirm("Esta gostando de JS?") janela com botão ok e cancelar
+// let nome = prompt("Qla é seu nome?") // vai perguntar o nome...
+// alert("E um grande prazer, " + nome)
+
+//let n1 = Number(prompt('digite um numero'))                // prompt sempre me retorna uma string
+//let n2 = Number(prompt('digite outro numero'))             // por isso vamos colocar Number.parseInt para inteiro 
+//let resultado = n1 + n2                                    // Number.parseFloat para numero real
+                                                           // So Number ele ja faz a soma exata
+
+
+//alert("A soma dos valores é " + resultado)    // Concatenação
+ 
+//   Para converter algo para string. ex: String(reultado)
+
+//   Number + Number | para adição
+//   String + String |  para concatenação 
+
+
+
+                                        //  USANDO TEMPLATESTRINGS
+
+// let nome = "Anderson"
+// let idade = 33
+// let nota = 9.5
+
+// console.log(`Meu nome é ${nome} tenho ${idade} anos, e tirei nota ${nota}`)
+
+
+
+                                               // FORMATANDO STRINGS
+
+   // EX: document.write(`Ola, <strong>${nome}</strong>! Seu nome tem ${nome.lenght} letras.`)
+
+   // EX: document.write(`Seu nome em maiúsculas é ${nome.toUpperCase()}`)
+
+   // EX: document.write(`Seu nome em minúsculas é ${nome.toLowerCase()}`)
+
+
+
+
+                                               //  FORMATANDO NUMEROS
+
+
+  //   n1.toFixed(2)
+  //   n1.toFixed(2)
+
+  //   n1.toLocaleString('pt-BR', {style: 'currency', currency: 'BRL'})
+  //   n1.toLocaleString('pt-BR', {style: 'currency', currency: 'EUR'})
+  //   n1.toLocaleString('pt-BR', {style: 'currency', currency: 'USD'})
