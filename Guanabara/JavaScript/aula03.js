159d7a707435cbdede4e6868577c69de0ef45076
Anderson Fernandes <a59399800@gmail.com>
2022-08-29 13:45:07 -0300

    alterado

---
+++ Guanabara/JavaScript/aula03.js
@@ -0,0 +1,56 @@
+// let numeros = [10,30,50,70]
+// let n2 =      [2,4,6,8,10]
+
+
+// dados recebido 
+// oque devo fazer com os dados
+// quais as restrições
+// qual resultado esperado
+//qual a sequencia de passos para resultado
+
+
+
+
+
+//-------------------------atribuindo notas no array e tirando uma media----------------------------------------
+// function notas(n1,n2,n3,n4){
+//   let r1 = n1+n2+n3+n4
+
+//   return r1
+// }
+// //console.log(notas(10,20,30,40))
+
+// function media(mediatotal){      // nome da função-----(mediatotal) => parametro
+ 
+//     let total = mediatotal/4
+ 
+//     return total
+// }
+// console.log(media(100)) // sempre chamar o nome da função depois parametro com a nota que quero
+
+
+
+
+
+//--------------------------------------transformando lista de array em numeros negativos-------------------------
+// let lista = [1,2,3,4,5,6,7,8,9,10]
+
+
+// function numerosnegativos(negativos){
+
+//     let capturar = []  // lugar para receber o push
+
+//     for(let i = 1; i <= negativos.length; i++){
+
+        
+  
+//         capturar.push(i*-1)  // puxando o indice(i) para novo array ja fazendo a conta
+        
+
+
+        
+//     }
+// return capturar
+// }
+// console.log(numerosnegativos(lista))
+
